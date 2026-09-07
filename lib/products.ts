5bca97f700f44ee963248939e2111e0c7d7c14f6
Soham Dandekar <90496440+sohamd008@users.noreply.github.com>
2026-09-07 16:29:09 +0530

    Remove GST information from business data

--- lib/products.ts
+++ lib/products.ts
@@ -18,7 +18,6 @@ export const business = {
   phoneDisplay: "+91 88888 51522",
   email: "swadamfoodsindia@gmail.com",
   address: "Lane No. 30/31 B, Ganesh Nagar, Dhayari, Pune",
-  gstin: "27AOCPD1930N1Z1",
   fssai: "21526080002094",
   udyam: "UDYAM-MH-26-1188295",
   legalEmail: "swadamfoodsindia@gmail.com",
