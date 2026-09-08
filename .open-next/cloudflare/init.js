5e99e0063490766a9016b71aeb1110e9900be89e
sohamd008 <sdanalt008@gmail.com>
2026-09-09 01:25:36 +0530

    chore: rebuild cloudflare worker bundle with admin + order tracker routes

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -47,7 +47,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1788894974191,
+    __BUILD_TIMESTAMP_MS__: 1788897283025,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
