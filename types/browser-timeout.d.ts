1062b08583ee2c563c96a031cffc2c9f15733cac
Soham Dandekar <90496440+sohamd008@users.noreply.github.com>
2026-09-15 21:21:12 +0530

    Remove obsolete browser timeout type workaround

--- types/browser-timeout.d.ts
+++
@@ -1,8 +0,0 @@
-export {}
-
-declare global {
-  interface Window {
-    setTimeout(handler: TimerHandler, timeout?: number, ...arguments: any[]): number
-    setTimeout(...args: any[]): any
-  }
-}
